87875e951b2f723d80d681f1467e4bd29ed182ff
prabhapachhai <pachhaiprabha5727@gmail.com>
2020-12-24 15:05:27 -0600

    initial commit

---
+++ Assint/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Assint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string yourName = "Prabha";
+            //Console.WriteLine("Hello:"+yourName);
+            //Console.WriteLine("sum of two number is:"+(4+6));
+            //Console.WriteLine("multification of two number is:"+(4*6));
+            //Console.WriteLine("divide of two number is:"+ (6/5));
+
+            int yourAge = 25;
+            Console.WriteLine("your age is :" + yourAge);
+            Console.WriteLine("you look older than 25");
+
+            // incremental order
+            //string yourname = "prabha";
+            //string topic = " learning c#";
+            //for (int i = 1; i <= 6; i++)
+            //{
+            //    //Console.WriteLine("hello:"+yourname);
+            //    //Console.WriteLine("****************************");
+            //    Console.WriteLine("topic:" + topic);
+            //    Console.WriteLine("**************************");
+
+
+               // descremental order
+
+                    string YourName = " prabha";
+                string language = "english";
+                for (int i = 6; i > 0; i--)
+                {
+                    Console.WriteLine("hello:" + YourName);
+                    Console.WriteLine("language:" + language);
+                }
+                //// reverse 
+                //int a, b, temp;
+                //Console.WriteLine("enter the value of a");
+                //a = int.Parse(Console.ReadLine());
+                //Console.WriteLine("enter the value of b");
+                //b = int.Parse(Console.ReadLine());
+                //Console.WriteLine("the value of a:{0}&b:{1}",a,b);
+                //temp = a;
+                //a = b;
+                //b = temp;
+                //Console.WriteLine("the value after reverse are a:{0}&b:{1}",a,b);
+                //Console.ReadKey();
+
+
+
+            }
+
+        }
+
+    }
+
+
+    
+
